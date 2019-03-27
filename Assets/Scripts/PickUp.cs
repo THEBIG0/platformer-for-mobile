6910437aae57000fb72dcb53b3332c417f3755cf
THEBIG0 <owendcgunter@gmail.com>
2019-03-27 16:34:28 +1030

    Inventory System
    player can pickup item, which is then stored to the inventory at bottom of screen.

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+	private Inventory inventory;
+	public GameObject itemButton;
+	BoxCollider2D box;
+    private void Start()
+    {
+		inventory = GameObject.FindGameObjectWithTag ("Player").GetComponent<Inventory> ();
+		box = GetComponent<BoxCollider2D> ();
+    }
+	void Update()
+	{
+		ItemEvent ();
+	}
+
+	private void ItemEvent()
+	{
+		if(box.IsTouchingLayers(LayerMask.GetMask("Player")))
+		{
+			for (int i = 0; i < inventory.slots.Length; i++) 
+			{
+				if (inventory.isFull [i] == false) 
+				{
+					inventory.isFull [i] = true;
+					Instantiate (itemButton, inventory.slots[i].transform, false);
+					Destroy (gameObject);
+					break;
+				}
+			}
+		}
+	}
+}
